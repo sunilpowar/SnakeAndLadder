682b377b7f3f009af5d6e4b60ad868345bb20f04
sunilpowar <sunilpowarsp8468@gmail.com>
2022-01-27 22:42:00 +0530

    adding Repeat player

--- SnakeAndLadder/SnakeAndLadder.cs
+++ SnakeAndLadder/SnakeAndLadder.cs
@@ -8,33 +8,42 @@ namespace SnakeAndLadder
 {
     internal class SnakeAndLadder
     {
-        public const int NO_PLAY = 0, SNAKE = 1, LADDER = 2;
+        public const int NO_PLAY = 0, SNAKE = 1, LADDER = 2, WINNING = 100;
         public int Start = 0;
         public int RolledDice;
         int Option = 0;
         public void GamePlayed()
         {
-            Random Random = new Random();
-            RolledDice = Random.Next(1, 7);
-            Console.WriteLine("player Roll  Dice number is:" + " " + RolledDice);
-            Option = Random.Next(0, 3);
-            Console.WriteLine("Dice value:" + Option);
-            switch (Option)
+            while (this.Start < 100)
             {
-                case NO_PLAY:
-                    Console.WriteLine("No Play");
-                    break;
-                case SNAKE:
-                    Start += RolledDice;
-                    Console.WriteLine("Got Ladder:" + Start);
-                    break;
-                case LADDER:
-                    Start -= RolledDice;
-                    Console.WriteLine("Snake Attack:" + Start);
-                    break;
-                default:
-                    Console.WriteLine("Invalid Option");
-                    break;
+                Random Random = new Random();
+                RolledDice = Random.Next(1, 7);
+                Console.WriteLine("player Roll  Dice number is:" + " " + RolledDice);
+                Option = Random.Next(0, 3);
+                Console.WriteLine("Dice value:" + Option);
+                switch (Option)
+                {
+                    case NO_PLAY:
+                        Console.WriteLine("No Play");
+                        break;
+                    case SNAKE:
+                        Start += RolledDice;
+                        Console.WriteLine("Got Ladder:" + Start);
+
+                        break;
+                    case LADDER:
+                        Start -= RolledDice;
+                        Console.WriteLine("Snake Attack:" + Start);
+                        break;
+                    default:
+                        Console.WriteLine("Invalid Option");
+                        break;
+                }
+                if (Start < 0)
+                {
+                    Start = 0;
+                }
+
             }
         }
     }
