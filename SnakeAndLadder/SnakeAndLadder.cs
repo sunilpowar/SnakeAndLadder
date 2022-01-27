e22c6496479af696a1607e9998fc237c7f99cde4
sunilpowar <sunilpowarsp8468@gmail.com>
2022-01-27 22:45:53 +0530

    Report no of Times

--- SnakeAndLadder/SnakeAndLadder.cs
+++ SnakeAndLadder/SnakeAndLadder.cs
@@ -12,33 +12,43 @@ namespace SnakeAndLadder
         public int Start = 0;
         public int RolledDice;
         int Option = 0;
+        int count = 0;
         public void GamePlayed()
         {
-            while (this.Start < 100)
+            while (this.Start != 100)
             {
+                count++;
                 Random Random = new Random();
                 RolledDice = Random.Next(1, 7);
+
                 Console.WriteLine("player Roll  Dice number is:" + " " + RolledDice);
                 Option = Random.Next(0, 3);
-                Console.WriteLine("Dice value:" + Option);
+                Console.WriteLine("option value:" + Option);
                 switch (Option)
                 {
                     case NO_PLAY:
                         Console.WriteLine("No Play");
                         break;
                     case SNAKE:
-                        Start += RolledDice;
-                        Console.WriteLine("Got Ladder:" + Start);
-                        break;
-                    case LADDER:
                         Start -= RolledDice;
                         Console.WriteLine("Snake Attack:" + Start);
+                        break;
+                    case LADDER:
+                        Start += RolledDice;
+                        Console.WriteLine("Got Ladder:" + Start);
                         break;
                     default:
                         Console.WriteLine("Invalid Option");
                         break;
                 }
+                if (Start < 0)
+                {
+                    Start = 0;
+                }
             }
+            Console.WriteLine("Number of times rolled dice " + count);
+            Console.WriteLine("successful");
+
         }
     }
 }
