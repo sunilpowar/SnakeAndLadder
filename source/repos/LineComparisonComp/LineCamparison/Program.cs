de0e0d82eecde99d27dcba680182a84a6d7a5852
sunilpowar <sunilpowarsp8468@gmail.com>
2022-01-27 22:20:07 +0530

    Adding Game

---
+++ source/repos/LineComparisonComp/LineCamparison/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparison
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome ");
+            LineComparison lc = new LineComparison();
+            lc.length_line();
+            lc.Comparelines();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ source/repos/SnakeAndLadder/SnakeAndLadder/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeLadder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to snake Ladder");
+            Player player = new Player();
+            player.playGame();
+            Console.ReadLine();
+        }
+    }
+}
